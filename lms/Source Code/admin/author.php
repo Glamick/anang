68640936b5c4912fa355df1aa5e692c8141a2612
Glamick <127052411+Glamick@users.noreply.github.com>
2023-03-05 21:33:49 +0100

    Add files via upload

---
+++ lms/Source Code/admin/author.php
@@ -0,0 +1,26 @@
+<?php
+include('connect.php');
+session_start();
+include 'header.php';
+include('navbar.php');?>
+    <!-- login area start -->
+    <div class="login-area">
+        <div class="container">
+            <div class="login-box ptb--100"> 
+            	<form name="login_form" method="post" style="width: 850px;""> 
+        <div style="font-size: 20px; padding: 5%;">
+        Name : <b>Anang Glamick</b><br>
+				Email : <b>anangglamick@gmail.com</b><br>
+				Contact : <b>+237651264662</b><br>
+				Address  : <b>DLA, Cameroon</b><br><br>
+				Experience : <b>I am a Programmer having Experty in languages like PHP, Codeignitor, Laravel, and Android. having 6 years+ Expe
+				Note : <b><mark> 
+For students or anyone else who needs program or source code for thesis writing or any Professional Software Development, Website Development, Mobile Apps Development at affordable cost.</mark> </b><br>
+				</div>
+
+    </form>
+            </div>
+        </div>
+    </div>
+    <!-- login area end -->
+    <?php include('footer.php');?>
---
+++ lms/Source Code/author.php
@@ -0,0 +1,26 @@
+<?php
+include('connect.php');
+session_start();
+include 'header.php';
+include('navbar.php');?>
+    <!-- login area start -->
+    <div class="login-area">
+        <div class="container">
+            <div class="login-box ptb--100"> 
+            	<form name="login_form" method="post" style="width: 850px;""> 
+        <div style="font-size: 20px; padding: 5%;">
+        Name : <b>Anang Glamick</b><br>
+				Email : <b>anangglamick@gmail.com.com</b><br>
+				Contact : <b>+237651264662</b><br>
+				Address  : <b>DLA, Cameroon</b><br><br>
+				Experience : <b>I am a student.</b><br><br>
+				School : <b>
+IUGET BONAMOUSADI.</b><br>
+				</div>
+
+    </form>
+            </div>
+        </div>
+    </div>
+    <!-- login area end -->
+    <?php include('footer.php');?>
